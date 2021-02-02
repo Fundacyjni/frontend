13544ac7d7fdc15025762fd0f4b8914a9be9385a
Wuzado <23276409+Wuzado@users.noreply.github.com>
2021-02-02 20:15:24 +0100

    feat(layout): create a responsive layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,34 +1,42 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer v-model="drawer" fixed app>
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title> Fundacyjni </v-toolbar-title>
+  <v-app>
+    <v-app-bar color="primary" fixed app>
+      <v-toolbar-title class="white--text"> Fundacyjni </v-toolbar-title>
+      <v-spacer />
+      <v-btn
+        v-for="item in items"
+        :key="item.title"
+        class="ml-5 d-none d-md-flex"
+        nuxt
+        :to="item.to"
+      >
+        <v-icon left>{{ item.icon }}</v-icon>
+        {{ item.title }}
+      </v-btn>
+      <v-btn class="ml-5 d-none d-md-flex" nuxt to="/login">
+        <v-icon left>mdi-login</v-icon>
+        Zaloguj się
+      </v-btn>
     </v-app-bar>
-    <v-main>
-      <v-container>
+    <v-main style="background: #f5f5f5">
+      <v-container style="height: 100%">
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-bottom-navigation
+      class="d-sm-flex d-md-none"
+      app
+      background-color="primary"
+      dark
+    >
+      <v-btn v-for="item in items" :key="item.title" nuxt :to="item.to">
+        <span>{{ item.title }}</span>
+
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+    <v-footer color="primary">
+      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
@@ -37,12 +45,21 @@
 export default {
   data() {
     return {
-      drawer: false,
       items: [
         {
           icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'Strona główna',
           to: '/',
+        },
+        {
+          icon: 'mdi-gift',
+          title: 'Podaruj rzeczy',
+          to: '/gifting',
+        },
+        {
+          icon: 'mdi-hand-heart',
+          title: 'Podaruj pieniądze',
+          to: '/fundraising',
         },
       ],
     }
