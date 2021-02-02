2b44429a1174b2c7b5d119e86a193306539e335d
Wuzado <23276409+Wuzado@users.noreply.github.com>
2021-02-02 20:16:41 +0100

    feat: add mocks for post/offer views

---
+++ components/itemCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card>
+    <v-img :src="image" max-height="200px"></v-img>
+    <v-card-title>Lorem Ipsum</v-card-title>
+    <v-card-subtitle> <v-img></v-img> przez Lorem Ipsum </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+  },
+}
+</script>
